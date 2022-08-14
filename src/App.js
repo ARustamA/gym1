d1a8b0dd4adeafcfa8847e5db474ec5fb9b963ff
“ARustamA” <rakhmetyanov@gmail.com>
2022-08-14 09:44:52 +0500

    11

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import style from './App.module.scss';
 function App() {
   return (
     <div className={style.app}>
-      <div className={style.containerTop}>
+      <div className={style.containerMain}>
+      
         <div className={style.header}>
           <a href='#' className={style.logo}></a>
           {/* <img src={logo} width={230} height={106} alt="logo" className={style.logo} /> */}
@@ -25,10 +26,22 @@ function App() {
             </li>
           </ul>
           <a href="tel:+74964161916" className={style.phone}>+7 (496) 416-19-16 </a>
+          </div>
+          <div className={style.content1}></div>
+          <div className={style.content2}></div>
+        <div className={style.title}>
+          <h1>Фитнес клуб Пантеон</h1>
+          <p>Лучше нас только боги</p>
+          <button className={style.titleButton}>
+          </button>
+          
         </div>
-
       </div>
 
+        
+          <div className={style.content3}></div>
+          <div className={style.content4}></div>
+          <div className={style.content5}></div>
 
       <div id='about'>
         <div className={style.container}>
@@ -57,7 +70,7 @@ function App() {
       </div>
       <a href='#' className={style.arroyUp}>
         <svg
-          
+
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
